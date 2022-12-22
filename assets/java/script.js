93049127b192c40da1551a1a045a308fcc1dbba1
JCSH <joshmatsbux@gmail.com>
2022-12-22 11:54:12 -1000

    fixed password store error

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,25 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var lCharSet= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var specSet= ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", ";", ":", "'", ",", "<", ".", ">", "/", "?","|"]
-var uCharSet= ["A", "B", "C", "D", "E","F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var finSet= []
-var passLength = 0
-var character = ""
-var pword = ""
+
 
 function generatePassword(){
-  for (i=0; i<=passLength; i++){
-    console.log (passLength)
-    character = finSet[Math.floor(Math.random() * finSet.length)]
-    pword += character
-  }
-  return pword
-}
-
-// Write password to the #password input
-function writePassword() {
+  var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var lCharSet= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var specSet= ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", ";", ":", "'", ",", "<", ".", ">", "/", "?","|"]
+  var uCharSet= ["A", "B", "C", "D", "E","F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+// var finSet= []
+  var passLength = 0
+  var character = ""
+  var pword = ""
+  var finSet= []
   passLength=prompt("How many characters will your password be?")
     if (passLength>128){
       alert("Password too long, please input an acceptable value")
@@ -45,7 +37,18 @@ function writePassword() {
   if (numCondition){
     finSet=[...finSet,...numSet]
   }
+  console.log(finSet)
 
+  for (i=0; i<=passLength; i++){
+    console.log (passLength)
+    character = finSet[Math.floor(Math.random() * finSet.length)]
+    pword += character
+  }
+  return pword
+}
+
+// Write password to the #password input
+function writePassword() {
   var password = generatePassword()
 
   var passwordText = document.querySelector("#password");
@@ -55,7 +58,6 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-console.log (pword)
 
+generateBtn.addEventListener("click", writePassword)
 
